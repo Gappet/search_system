bb6cd8a26f83bc5d05ee2e3f637ed4fb1ff43650
Vasilii Chupin <tchupin.vasilij@yandex.ru>
2020-11-26 20:10:22 +0300

    Rameke remove_duplicates

--- src/remove_duplicates.cpp
+++ src/remove_duplicates.cpp
@@ -10,22 +10,21 @@ using namespace std::string_literals;
 
 void RemoveDuplicates(SearchServer& search_server) {
   std::set<int> dublicates_docs;
-  for (auto it = search_server.begin(); it != search_server.end(); ++it) {
-    const std::map<std::string, double> doc_words =
-        search_server.GetWordFrequencies(*it);
-    for (auto it_int = next(it, 1); it_int != search_server.end(); ++it_int) {
-      const std::map<std::string, double> comp_doc_words =
-          search_server.GetWordFrequencies(*it_int);
-      if (doc_words.size() == comp_doc_words.size() &&
-          std::equal(doc_words.begin(), doc_words.end(), comp_doc_words.begin(),
-                     [](const auto& a, const auto& b) {
-                       return a.first == b.first;
-                     })) {
-        dublicates_docs.insert(*it_int);
-        break;
+
+    std::set<std::set<std::string>> cash;
+    for (auto it = search_server.begin(); it != search_server.end(); ++it) {
+      const std::map<std::string, double> doc_words =
+          search_server.GetWordFrequencies(*it);
+      std::set<std::string> words;
+      for (const auto& [key, _] : doc_words) {
+        words.insert(key);
+      }
+      if (cash.count(words)) {
+        dublicates_docs.insert(*it);
+      } else {
+        cash.insert(words);
       }
     }
-  }
 
   for (int i : dublicates_docs) {
     std::cout << "Found duplicate document id "s << i << std::endl;
